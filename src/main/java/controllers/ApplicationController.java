0cd8ef94369edae3ac0af990ad3f1a3d7f9d1c48
Ilya <ilya@iMbp2.local>
2017-04-13 17:03:49 +0700

    Use Todo class as method parameter

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import com.google.inject.Inject;
-import com.jayway.jsonpath.ReadContext;
 import io.mangoo.routing.Response;
 import io.mangoo.routing.bindings.Request;
 import jooq.tables.records.TodoRecord;
@@ -15,6 +14,12 @@ import static jooq.tables.Todo.TODO;
 public class ApplicationController {
     @Inject private DSLContext create;
 
+    public Response get(String id) {
+        System.out.println(id);
+
+        return Response.withOk().andEmptyBody();
+    }
+
     public Response getAll(Request r) {
         List<Todo> result = create.selectFrom(TODO).fetchInto(Todo.class);
 
@@ -23,18 +28,10 @@ public class ApplicationController {
         return Response.withOk().andJsonBody(result);
     }
 
-    public Response create(Request r) {
+    public Response create(Request r, Todo t) {
+        TodoRecord tr = create.insertInto(TODO).columns(TODO.TITLE).values(t.title).returning(TODO.ID).fetchOne();
 
-        ReadContext ctx = r.getBodyAsJsonPath();
-
-        String title = ctx.read("$.title");
-
-
-        TodoRecord tr = create.insertInto(TODO).columns(TODO.TITLE).values(title).returning(TODO.ID).fetchOne();
-
-        Todo t = new Todo();
         t.id = tr.get(TODO.ID);
-        t.title = title;
         t.url = composeUrl(r, t.id);
 
         return Response.withOk().andJsonBody(t);
@@ -63,4 +60,11 @@ public class ApplicationController {
 
     //GET: / - returns all as an array
     //[{"id":1,"title":"walk the dog","completed":false,"order":0,"url":"https://todo-backend-spring4-java8.herokuapp.com/todos/1"}]
+
+//    Request URL:http://todobackend-mangooio.herokuapp.com/5
+//    Request Method:OPTIONS
+//    Status Code:404 Not Found
+
+    //GET: http://todobackend-spring.herokuapp.com//4
+    //{"title":"my todo","completed":false,"url":"http://todobackend-spring.herokuapp.com//4","order":null}
 }
